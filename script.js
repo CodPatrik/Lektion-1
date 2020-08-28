82032f822a17a6adc9c941c06aefd6d8ed0d07df
CodPatrik <54668490+CodPatrik@users.noreply.github.com>
2020-08-28 12:45:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// I denna fil skriver vi javascript kod
+
+// Deklarera variabler
+let length = 5;
+let width = 10;
+let height = 20;
+
+// Deklarera en funktion "ett litet program i programmet"
+function calculateVolume(){
+
+    let volume = length*width*height
+    console.log(volume);
+
+}
+
+
+// Anropa "köra" en funktion
+calculateVolume();
